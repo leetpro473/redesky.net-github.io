b586edd7e77d077a96f9109750d30ee4b807e86a
leetpro473 <80022664+leetpro473@users.noreply.github.com>
2021-10-11 19:43:45 +0000

    Add files via upload

---
+++ app/templates/site/assets/js/app.js
@@ -0,0 +1,2 @@
+var owl=$('.owl-carousel');if(owl.length){owl.owlCarousel({margin:10,responsiveClass:true,responsive:{0:{items:1,nav:true,dots:true,},600:{items:2,nav:false,dots:true},1000:{items:4,nav:true,loop:false,dots:true}}});}
+$('[data-toggle="tooltip"]').tooltip();
